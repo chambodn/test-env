5fbc4f9ed9c3faefa305dee64f86433782a81a02
Damien Chambon <damien.chambon@nexthink.com>
2022-08-29 09:54:41 +0200

    test

--- .github/helper/src/function.js
+++ .github/helper/src/function.js
@@ -1,8 +1,18 @@
 export function printStuff() { console.log('stuff') }
 export function checkEnv() { console.log('env') }
-export function stringToList(changedFiles) {
-    if(changedFiles) {
-        return changedFiles.split(';');
+export function changedFilesByEnv(strSandboxChangedFiles, strDevChangedFiles,
+    strPreChangedFiles, strProdChangedFiles) {
+
+    const filesPerEnv = new Map()
+    filesPerEnv.set('sandbox', strSandboxChangedFiles.split(','))
+    filesPerEnv.set('dev', strDevChangedFiles.split(','))
+    filesPerEnv.set('pre', strPreChangedFiles.split(','))
+    filesPerEnv.set('prod', strProdChangedFiles.split(','))
+    return filesPerEnv
+}
+export function getIncorrectListOfChangedFiles() {
+    if (changedFiles) {
+        return changedFiles.split(',');
     }
     return [];
 }
