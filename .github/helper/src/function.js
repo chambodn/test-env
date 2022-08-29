eb68637d305c557d3bc55b01f001ae6f93b14408
Damien Chambon <damien.chambon@nexthink.com>
2022-08-29 08:46:45 +0200

    test

--- .github/helper/src/function.js
+++ .github/helper/src/function.js
@@ -1,2 +1,8 @@
 export function printStuff() { console.log('stuff') }
 export function checkEnv() { console.log('env') }
+export function stringToList(changedFiles) {
+    if(changedFiles) {
+        return changedFiles.split(';');
+    }
+    return [];
+}
